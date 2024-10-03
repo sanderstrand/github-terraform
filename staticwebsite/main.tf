3a6a903b218640793eb8bb02aad481ef62e72c23
sanderstrand <sander_H_S@hotmail.com>
2024-10-03 14:55:55 +0200

    test

--- staticwebsite/main.tf
+++ staticwebsite/main.tf
@@ -2,7 +2,7 @@ locals {
     workspace_suffix = terraform.workspace == "default" ? "" : "${terraform.workspace}"
 
     rg_name = terraform.workspace == "default" ? "${var.rg_name}" : "${var.rg_name}-${local.workspace_suffix}"
-    sa_name = "${var.sa_name}${local.workspace_suffix}"
+    sa_name = terraform.workspace == "default" ? "${var.sa_name}" : "${var.sa_name}${local.workspace_suffix}"
     web_suffix = "<h1>${terraform.workspace}</h1>"
 }
 
