7b7eaed3c2990014b3fd94372ff96069ea1297fe
sanderstrand <sander_H_S@hotmail.com>
2024-10-03 16:18:59 +0200

    test10

--- staticwebsite/main.tf
+++ staticwebsite/main.tf
@@ -42,6 +42,8 @@ resource "azurerm_storage_blob" "index_html" {
   content_type           = "text/html"
   source_content         = "${var.source_content}${local.web_suffix}"
   #source_content         = "<h1>Web page created with terraform - helt splitter ny</h1>"
+  content_md5 = filebase64md5("${path.module}/index.html")  # Calculates the MD5 hash of the file content
+
 }
 #kommentar
 
